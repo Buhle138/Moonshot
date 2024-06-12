1d7079c7a2afccbddc5d489f9ad0ff9af0848385
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-12 20:01:00 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Buhle Radzilani on 2024/06/12.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
